9c359bf6a74b993976708dbdde223f1e5e77d394
DanielD26 <103547416@student.swin.edu.au>
2022-11-24 15:34:16 +1100

    finished unit testing

--- Backend/Tests/UnitTest1.cs
+++ Backend/Tests/UnitTest1.cs
@@ -1,10 +1,28 @@
 namespace Tests;
 
-[TestClass]
+using API;
+using Xunit;
+
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
-    {
+    [Theory]
+    [InlineData(5, 2.4)]
+    [InlineData(10, 1.5)]
+    [InlineData(2, 5.7)]
+    // Test for calculate total
+    public void TotalCostTest(int q, float price) {
+        var order = new Order().calcTotal(q, price);
+        var result = q * price;
+        Assert.Equal(order, result);
+    }
+    // Test for GST
+    [Theory]
+    [InlineData(5, 2.4)]
+    [InlineData(10, 1.5)]
+    [InlineData(2, 5.7)]
+    public void gstTest(int q, float price) {
+        var order = new Order().gst(q, price);
+        var result = (q * price)/10;
+        Assert.Equal(order, result);
     }
 }
